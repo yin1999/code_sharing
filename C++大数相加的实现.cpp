6de7be18148c9e6202428cfdb7ca95b5026884f7
yinyitao <yin199909@gmail.com>
2018-10-12 13:44:59 +0800

    Update C++大数相加的实现.cpp

--- C++大数相加的实现.cpp
+++ C++大数相加的实现.cpp
@@ -2,9 +2,9 @@
 #include<string>
 using namespace std;
 
-string addup(string num1, string num2)
+string addup(string num1, string num2)    //定义大数相加函数
 {
-	if (num1.size() < num2.size())
+	if (num1.size() < num2.size())    //判断两数长度，更长的放在num1，方便计算
 	{
 		string temp = num1;
 		num1 = num2;
@@ -13,7 +13,7 @@ string addup(string num1, string num2)
 	int length1 = num1.size(), length2 = num2.size(), flag = 0, sum, a, b;
 	while (length1 > 0)
 	{
-		a = num1[length1 - 1] - '0';
+		a = num1[length1 - 1] - '0';        //ASCII码运算处理，从字符中提取数字
 		if (length2 > 0)
 			b = num2[length2 - 1] - '0';
 		else b = 0;
@@ -32,10 +32,10 @@ string addup(string num1, string num2)
 		length2--;
 	}
 	if (flag == 1)
-		num1 = "1" + num1;
+		num1 = "1" + num1;     //若最高位要进一，则通过这一步实现
 	return num1;
 }
-int main()
+int main()      //main函数为杭电acm的问题1002
 {
 	int t, k;
 	string num1, num2;
