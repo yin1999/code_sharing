69ddfbcf1ebba0a54150fa12c2c74af220298729
yinyitao <yin199909@gmail.com>
2018-10-21 15:47:38 +0800

    Create 选择排序示例.cpp

---
+++ 排序算法/选择排序示例.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	int a[8] = { 3,1,4,3,7,8,5,6 };
+	for (int i = 0; i < 7; i++)
+	{
+		int temp = i;
+		for (int j=i+1;j<7;j++)
+		{
+			if (a[temp] > a[j])
+				temp = j;
+		}
+		int coach = a[i];
+		a[i] = a[temp];
+		a[temp] = coach;
+	}
+	for (int c = 0; c < 8; c++)
+		cout << a[c] << " ";
+	return 0;
+}
