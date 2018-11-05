3bc788a3042fe74eceff47f64846943a80b3f35e
yinyitao <yin199909@gmail.com>
2018-11-05 15:11:54 +0800

    Update T5.cpp

--- school_work/第一学期/5/T5.cpp
+++ school_work/第一学期/5/T5.cpp
@@ -1,16 +1,14 @@
-﻿#include<iostream>
-#include<cstdlib>
-#include<ctime>
+#include<iostream>
 using namespace std;
 
 int main()
 {
 	int a[1000];
-	int i = 0;
+	int i = 0;  //储存输入了几个数
 	cout << "输入\'Ctrl+z\'结束输入，最多输入1000个数" << endl;
-	while (cin >> a[i])
+	while (cin >> a[i])   //Ctrl+z是结束输入流
 	{
-		i++;
+		i++;   //计数器自增
 	}
 	cout << "其逆序数为" << endl;
 	while (i--)
