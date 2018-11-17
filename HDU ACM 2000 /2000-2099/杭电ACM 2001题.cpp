b41d7325b8173c0b026d5b08bad90de3613ab327
yinyitao <15844309+yin1999@users.noreply.github.com>
2018-11-17 16:14:16 +0800

    Update 杭电ACM 2001题.cpp

--- HDU ACM 2000 /2000-2099/杭电ACM 2001题.cpp
+++ HDU ACM 2000 /2000-2099/杭电ACM 2001题.cpp
@@ -3,14 +3,14 @@
 #include<iomanip>
 using namespace std;
 
-typedef struct point  //定义point类型，储存x，y坐标
+struct point  //定义point类型，储存x，y坐标
 {
 	float x, y;
 }point;
 
 int main()
 {
-	point *points = new point[2];
+	point point[2];
 	while (cin >> points[0].x >> points[0].y >> points[1].x >> points[1].y)
 	{
 		cout << fixed << setprecision(2) << sqrt((points[0].x - points[1].x)*(points[0].x - points[1].x) + (points[0].y - points[1].y)*(points[0].y - points[1].y)) << endl;
