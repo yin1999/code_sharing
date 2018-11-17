1906026e0a3d15d6f4095cc7c0b4df9a12fc79db
yinyitao <15844309+yin1999@users.noreply.github.com>
2018-11-17 15:02:23 +0800

    Create 示例.cpp

---
+++ ESTA/2018.11现场编程大赛/问题D-总和问题/示例.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	int a;
+	for (int i = (int)sqrt(2*m); i >= 1; i--)
+	{
+		a = a = (m - ((i - 1)*i) / 2) / i;
+		if (m == a * i + (i*(i - 1)) / 2 && a > 0)
+			if (a + i - 1 <= n)
+				cout << "[" << a << " , " << a + i - 1 << "]" << endl;
+	}
+	return 0;
+}
