914ce0ae5a0ec1ef4f68d60e511aee3a5845ad9f
yinyitao <15844309+yin1999@users.noreply.github.com>
2018-11-26 21:49:45 +0800

    Create P1003.cpp

---
+++ 洛谷/P1000-1099/P1003.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int a[10000][4];
+int main()
+{
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < 4; j++)
+			cin >> a[i][j];
+	}
+	int x, y, count1 = -2;
+	cin >> x >> y;
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i][0]<=x&&a[i][1]<=y&&a[i][0] + a[i][2]>=x&&a[i][1] + a[i][3]>=y)
+			count1 = i;
+	}
+	cout << count1 + 1;
+	return 0;
+}
