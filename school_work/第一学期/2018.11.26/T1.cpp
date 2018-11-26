a4f81f270b87dda92e57d25ccb8aaa7c395375c3
yinyitao <15844309+yin1999@users.noreply.github.com>
2018-11-26 15:07:48 +0800

    Update T1.cpp

--- school_work/第一学期/2018.11.26/T1.cpp
+++ school_work/第一学期/2018.11.26/T1.cpp
@@ -6,12 +6,12 @@ int getint(char *a, int *b)
 	int count1 = 0;
 	int num = 0;
 	bool flag = false;
-	while (*a)
+	while (*a)      //判断是否到了字符串结尾
 	{
-		if (*a >= '0'&&*a <= '9')
+		if (*a >= '0'&&*a <= '9')     //判断是否为数字
 		{
-			num = num * 10 + *a - '0';
-			flag = true;
+			num = num * 10 + *a - '0';     //取出数字，加到num中
+			flag = true;    //记录当前数为数字（考虑下一位也是数字或者不是数字的情况
 		}
 		else if(flag)
 		{
@@ -19,18 +19,19 @@ int getint(char *a, int *b)
 			num = 0;
 			flag = false;
 		}
-		a++;
+		a++;    //让目前判断的字符后移一位
 	}
 	if (flag)
-		b[count1++] = num;
-
-	return count1;
+		b[count1++] = num;     //不要忘记判断最后一个非空字符是数字的情况
+	
+	return count1;    //返回整数个数
 }
 
 int main()
 {
 	char a[1000];
 	int b[100];
+	cout << "请输入任意字符串，输入Ctrl+z终止输入流" << endl;
 	while (cin.getline(a,1000))
 	{
 		int n = getint(a, b);
