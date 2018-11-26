a0ee906ff9f1e6fffe5e63daeddd89ec7e39db4d
yinyitao <15844309+yin1999@users.noreply.github.com>
2018-11-26 15:29:34 +0800

    Update T3.cpp

--- school_work/第一学期/2018.11.26/T3.cpp
+++ school_work/第一学期/2018.11.26/T3.cpp
@@ -5,12 +5,12 @@ using namespace std;
 typedef struct dataline
 {
 	int a[3];
-	dataline * next;
+	dataline * next;   //记录下一个结构体的地址
 } datatype;
 
 int main()
 {
-	datatype* head = new datatype;
+	datatype* head = new datatype;   //分配空间，并把地址存入head
 	datatype* q = head;
 	if (head == NULL)
 		return -1;
@@ -19,7 +19,7 @@ int main()
 	cout << "请输入一个数组，输入Ctrl+z终止输入流" << endl;
 	while (cin >> temp)
 	{
-		if (count1++ == 2)
+		if (count1++ == 2)   //如果输入了3个数而又要继续输入了，则new一个新的结构体
 		{
 			datatype* t = new datatype;
 			if (t == NULL)
@@ -45,11 +45,11 @@ int main()
 	cout << endl;
 
 	q = head;
-	while (q != 0)
+	while (q != 0)   //判断指向地址是否为0，若为零，则说明delete完成（这个循环连起来看）
 	{
-		datatype* next = q->next;
-		delete q;
-		q = next;
+		datatype* next = q->next;   //先储存下一个结构体的首地址
+		delete q;   //delete
+		q = next;    //使q指向下一个结构体地址
 	}
 	return 0;
 }
