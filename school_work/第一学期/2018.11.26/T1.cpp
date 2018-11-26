29640448ffab9fd560cbfd78e74efe7cc731b43f
yinyitao <15844309+yin1999@users.noreply.github.com>
2018-11-26 15:13:44 +0800

    Update T1.cpp

--- school_work/第一学期/2018.11.26/T1.cpp
+++ school_work/第一学期/2018.11.26/T1.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int getint(char *a, int *b)
 {
-	int count1 = 0;
-	int num = 0;
-	bool flag = false;
+	int count1 = 0;   //记录整数个数，初始化为0
+	int num = 0;      //记录字符串内的数
+	bool flag = false;   //记录前一位是否为数字，若为数字，记录为true，初始化为false
 	while (*a)      //判断是否到了字符串结尾
 	{
 		if (*a >= '0'&&*a <= '9')     //判断是否为数字
@@ -13,11 +13,11 @@ int getint(char *a, int *b)
 			num = num * 10 + *a - '0';     //取出数字，加到num中
 			flag = true;    //记录当前数为数字（考虑下一位也是数字或者不是数字的情况
 		}
-		else if(flag)
+		else if(flag)   //前一位为数字，当前位不是数字的情况
 		{
-			b[count1++] = num;
-			num = 0;
-			flag = false;
+			b[count1++] = num;    //将怒骂储存到b数组中
+			num = 0;      //初始化位0
+			flag = false;    //赋值为false
 		}
 		a++;    //让目前判断的字符后移一位
 	}
