6d6bb9b223f50a492d542357cf85fb5912d6726a
yinyitao <15844309+yin1999@users.noreply.github.com>
2018-11-26 14:49:05 +0800

    Create T2.cpp

---
+++ school_work/第一学期/2018.11.26/T2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void transfer(char *a, char (*m)[12],int ms)
+{
+	int n = 1;
+	char *p = m[ms];
+	a[0] = *p;
+	while (*p++)
+		a[n++] = *p;
+}
+
+int main()
+{
+	char m[][12] = { "January","February","March","April","May","June","July","August","September","October","November","December" };
+	char out[12];
+	int n;
+	while (cin >> n)
+	{
+		if (n <= 0 || n > 12)
+		{
+			cout << "输入数据不合法" << endl;
+			continue;
+		}
+
+		transfer(out, m, n - 1);
+		cout << "月份为 " << out << endl;
+	}
+	return 0;
+}
