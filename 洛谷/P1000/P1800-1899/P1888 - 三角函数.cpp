5944b835132e876f896546301cff8719787d91d9
yinyitao <15844309+yin1999@users.noreply.github.com>
2018-11-29 19:48:17 +0800

    Create P1888 - 三角函数.cpp

---
+++ 洛谷/P1000/P1800-1899/P1888 - 三角函数.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+void sort1(int &a, int &b)
+{
+    if(a < b)
+    {
+        int t = b;
+        b = a;
+        a = t;
+    }
+}
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    sort1(b, c);
+    sort1(a, b);
+    sort1(b, c);
+    int max1 = a;
+    int min1 = c;
+    while(max1 != min1)  //辗转相减法求最大公约数
+	{
+	    if(max1 > min1)
+		{
+			max1 -= min1;
+		}
+		else
+		{
+			min1 -= max1;
+		}
+	}
+    cout << c / max1 << "/" << a / min1 << endl;
+    return 0;
+}
