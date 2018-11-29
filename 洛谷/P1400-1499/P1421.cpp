2d3d1c0bfde8dc09bd5dffaf90f1a8c907c5fb74
yinyitao <15844309+yin1999@users.noreply.github.com>
2018-11-29 14:35:43 +0800

    Create P1421.cpp

---
+++ 洛谷/P1400-1499/P1421.cpp
@@ -0,0 +1,12 @@
+//价格乘以十再计算就很简单了
+#include <iostream>
+#define price 19
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    a = a * 10 + b;
+    cout << a / price << endl;
+}
