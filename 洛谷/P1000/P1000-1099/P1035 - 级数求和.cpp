c988f53aec0a275eb0485654115daa8b17a40026
yinyitao <15844309+yin1999@users.noreply.github.com>
2018-12-09 21:48:42 +0800

    Create P1035 - 级数求和.cpp

---
+++ 洛谷/P1000/P1000-1099/P1035 - 级数求和.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n = 0, k;
+	double sum = 0;
+	cin >> k;
+	while (k >= sum)
+	{
+		n++;
+		sum += 1. / n;
+	}
+	cout << n << endl;
+	return 0;
+}
