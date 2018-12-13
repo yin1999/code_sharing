d55f87cd2219237d906070f027a104cc3529ce2a
yinyitao <15844309+yin1999@users.noreply.github.com>
2018-12-13 15:10:13 +0800

    Create P1075 - 质因数分解.cpp

---
+++ 洛谷/P1000/P1000-1099/P1075 - 质因数分解.cpp
@@ -0,0 +1,19 @@
+//题目给出的条件，输入的数只能被分解成两个质数，所以，找到能整除的数直接输出即可
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            cout << n / i << endl;
+            return 0;
+        }
+    }
+    return 0;
+}
