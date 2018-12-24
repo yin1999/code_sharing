dc28fc4015b20402ecf987ea7ca4b48e8315b282
yinyitao <15844309+yin1999@users.noreply.github.com>
2018-12-24 19:39:44 +0800

    Create P1047 - 校门外的树.cpp

---
+++ 洛谷/P1000/P1000-1099/P1047 - 校门外的树.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int L, M, i;
+    cin >> L >> M;
+    
+    int *a = new int[L + 1];   //开辟数组，初始化为1
+    for (i = 0; i <= L; i++)
+        a[i] = 1;
+        
+    int left, right;
+    while (M--)
+    {
+        cin >> left >> right;
+        for (i = left; i <= right; i++)
+            a[i] = 0;
+    }
+    
+    int sum = 0;
+    for (i = 0; i <= L; i++)   //求和，为1的就是那棵树还在
+        sum += a[i];
+        
+    delete[] a;
+    
+    cout << sum << endl;
+    return 0;
+}
