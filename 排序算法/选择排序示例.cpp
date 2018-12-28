c3db543f56dcbf52afbdf7d720cf4ade7e8be49a
yinyitao <15844309+yin1999@users.noreply.github.com>
2018-12-28 09:42:35 +0800

    Update 选择排序示例.cpp

--- 排序算法/选择排序示例.cpp
+++ 排序算法/选择排序示例.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main()
 {
 	int a[8] = { 3,1,4,3,7,8,5,6 };
-	for (int i = 0; i < 7; i++)
+	for (int i = 0; i < 8; i++)
 	{
 		int temp = i;
-		for (int j=i+1;j<7;j++)
+		for (int j = i + 1; j < 8; j++)
 		{
 			if (a[temp] > a[j])
 				temp = j;
