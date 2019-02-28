cef0a9837c1ce50a9f7635172a0768ff8e75aca6
yinyitao <15844309+yin1999@users.noreply.github.com>
2019-02-28 09:52:12 +0800

    Update manage.h

--- Project1/Project1/manage.h
+++ Project1/Project1/manage.h
@@ -1,7 +1,13 @@
 #pragma once
-#include <iostream>
+#include <fstream>
 #include <cstdlib>
+#include <string>
+#include <cstring>
 #include "user.h"
 #include "main.h"
+#include "device.h"
 
 void manage(user& User_data);
+
+extern bool user_change;
+extern bool device_change;
