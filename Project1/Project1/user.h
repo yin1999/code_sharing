a6d147ca274e6bb16b676824f94e4fecd7aa6b79
yinyitao <15844309+yin1999@users.noreply.github.com>
2019-03-04 09:54:45 +0800

    Update user.h

--- Project1/Project1/user.h
+++ Project1/Project1/user.h
@@ -1,109 +1,35 @@
 #pragma once
 #include <iostream>
-#include <cstring>
+#include <fstream>
 #include <string>
 #include <conio.h>
-#include <cstdio>
+#include <Windows.h>
 using namespace std;
+
+class Account
+{
+public:
+	string username;
+	string passwd;
+	Account operator =(Account&);
+	bool operator ==(Account&);
+	bool GetAccount();
+};
 
 class user
 {
 public:
-	char Username[16];
-	char Passwd[16];
+	Account account;
 	string name;
 	string area;
-	bool Primer;
+	bool primer;
 	bool judged;
 	bool deleted;
-	user() { Username[0] = 0, Passwd[0] = 0, Primer = false, judged = false, deleted = false; }
+	user() { primer = false, judged = false, deleted = false; }
 	user(user&);
-	user operator= (user&);
+	void operator= (user&);
 	void GetUserMsg(int);
-	friend istream& operator >> (istream&, user&);
+	friend ifstream& operator >> (ifstream&, user&);
+	friend ofstream& operator << (ofstream&, user&);
+	friend ostream& operator << (ostream&, user&);
 };
-
-user::user(user& T)
-{
-	strcpy_s(Username, T.Username);
-	strcpy_s(Passwd, T.Passwd);
-	name = T.name;
-	area = T.area;
-	Primer = T.Primer;
-	judged = T.judged;
-	deleted = T.deleted;
-}
-
-user user::operator=(user& T)
-{
-	strcpy_s(Username, T.Username);
-	strcpy_s(Passwd, T.Passwd);
-	name = T.name;
-	area = T.area;
-	Primer = T.Primer;
-	judged = T.judged;
-	deleted = T.deleted;
-	return *this;
-}
-
-void user::GetUserMsg(int flag)
-{
-	cout << "用户名(长度限制15个字符):";
-	char temp;
-	int i = 0;
-	while ((temp = getchar()) != '\n')
-	{
-		if (i > 14)
-		{
-			i = 0;
-			while ((temp = getchar()) != '\n');
-			cout << "用户名不合法，请重新输入\n";
-		}
-		else
-			Username[i++] = temp;
-	}
-	Username[i] = 0;
-	cout << "密码(长度限制15个字符):";
-	i = 0;
-	while ((temp = _getch()) != '\r')
-	{
-		if (temp == 8)
-		{
-			if (i != 0)
-			{
-				cout << char(8) << ' ' << char(8);
-				i--;
-			}
-		}
-		else
-		{
-			putchar('*');
-			if (i > 14)
-			{
-				while ((temp = getchar()) != '\n');
-				cout << "输入密码不合法，请重新输入\n";
-			}
-			else
-				Passwd[i++] = temp;
-		}
-	}
-	Passwd[i] = 0;
-	if (flag)
-	{
-		Primer = true;
-		judged = true;
-	}
-	else
-	{
-		Primer = false;
-		judged = false;
-	}
-	deleted = false;
-	putchar('\n');
-}
-
-istream& operator >>(istream& input, user& T)
-{
-	input >> T.Username >> T.Passwd >> T.name >> T.area >> T.Primer >> T.judged;
-	return input;
-}
