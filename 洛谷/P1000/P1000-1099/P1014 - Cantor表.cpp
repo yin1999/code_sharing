d9503b02cd3efe136815ac5da5b73007329c9357
yinyitao <15844309+yin1999@users.noreply.github.com>
2019-04-15 16:15:10 +0800

    Create P1014 - Cantor表.cpp

---
+++ 洛谷/P1000/P1000-1099/P1014 - Cantor表.cpp
@@ -0,0 +1,19 @@
+//规律，先按第10行到第13行的循环算出对应的行数与对应第几个数(每行数对应增加1)，每行的分子分母之和为(行数加一)
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int i = 1;
+	while (i < n)
+	{
+		n -= i++;
+	}
+	if ((i & 1) == 0)
+		cout << n << '/' << i + 1 - n << endl;
+	else
+		cout << i + 1 - n << '/' << n << endl;
+	return 0;
+}
