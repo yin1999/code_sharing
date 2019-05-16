55d86f8a007293a3fd91a2639389eb3d84682b83
yinyitao <15844309+yin1999@users.noreply.github.com>
2019-05-16 16:06:18 +0800

    Update test.py

--- ESTA/物联网创想秀/test.py
+++ ESTA/物联网创想秀/test.py
@@ -1,17 +1,17 @@
 from Source import TextClassification
 import pickle
 
+## =============================
+## 导入训练数据
 with open('training.pickle', 'rb') as file:
     content_list = pickle.load(file)
 with open('trainlable.pickle', 'rb') as file:
     label_list = pickle.load(file)
+    
 model = TextClassification(content_list, label_list)
 model.trainModel()
-model.store_idList()
-#for i in range(5):
-    #S = input()
-    #L = [S]
-    #model.test(L)
-
-# model.printConfusionMatrix()
-# model.printReportTable()
+model.store_idList()   # 保存文字转id的字典列表
+## ============================
+## 输出统计数据
+model.printConfusionMatrix()
+model.printReportTable()
