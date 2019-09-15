0a72b9a5fb28380bc5387cde226c9af4cc5b4fec
Yin Yitao <15844309+yin1999@users.noreply.github.com>
2019-09-15 21:44:45 +0800

    Update 示例.cpp

--- 算法导论/5.1-2随机数生成/示例.cpp
+++ 算法导论/5.1-2随机数生成/示例.cpp
@@ -3,27 +3,22 @@
 #include <cstdlib>
 using namespace std;
 
-unsigned int rand_0_to_1()   //随机生成0或1
-{
+unsigned int rand_0_to_1() {  //随机生成0或1
     return (rand() & 1);
 }
 
-int RAND(unsigned int a, unsigned int b)   //a到b范围数随机生成
-{
+int RAND(unsigned int a, unsigned int b) {  //a到b范围数随机生成
     unsigned int n = b - a;
     unsigned int bits = 0;
     unsigned int temp = n;
-    while (temp)
-    {
+    while (temp) {
         temp >>= 1;
         bits++;
     }
     unsigned int result = n + 1;
-    while (result > n)
-    {
+    while (result > n) {
         result = 0;
-        for (int i = 0; i < bits; i++)
-        {
+        for (int i = 0; i < bits; i++) {
             result <<= 1;
             result += rand_0_to_1();
         }
@@ -31,11 +26,9 @@ int RAND(unsigned int a, unsigned int b)   //a到b范围数随机生成
     return result + a;
 }
 
-int main()
-{
+int main() {
     srand((unsigned int)time(NULL));
-    for (int i = 0; i < 100; i++)
-    {
+    for (int i = 0; i < 100; i++) {
         cout << RAND(0, 100) << '\t';   //随机生成0到100的无符号整数（可修改范围）
     }
     cout << endl;
