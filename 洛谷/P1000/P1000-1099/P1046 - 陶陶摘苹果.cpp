38745e793a535777ee1b11d475ec603fe722e716
Yin Yitao <15844309+yin1999@users.noreply.github.com>
2019-09-15 21:53:34 +0800

    Update P1046 - 陶陶摘苹果.cpp

--- 洛谷/P1000/P1000-1099/P1046 - 陶陶摘苹果.cpp
+++ 洛谷/P1000/P1000-1099/P1046 - 陶陶摘苹果.cpp
@@ -1,20 +1,18 @@
 #include<iostream>
 using namespace std;
 
-int main()
-{
+int main() {
     int a[10], n;
-    for (int i = 0; i < 10; i++)
-    {
+    for (int i = 0; i < 10; i++) {
         cin >> a[i];
     }
     cin >> n;
     int max_level = n + 30;    //最大高度
     int count1 = 0;   //计数
-    for (int i = 0; i < 10; i++)
-    {
-        if(max_level >= a[i])
+    for (int i = 0; i < 10; i++) {
+        if(max_level >= a[i]) {
             count1++;
+        }
     }
     cout << count1 << endl;
     return 0;
