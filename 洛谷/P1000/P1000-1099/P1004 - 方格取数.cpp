826e5420c0448723ffed55d931f16b8662ae2f01
Yin Yitao <15844309+yin1999@users.noreply.github.com>
2019-09-15 21:49:21 +0800

    Update P1004 - 方格取数.cpp

--- 洛谷/P1000/P1000-1099/P1004 - 方格取数.cpp
+++ 洛谷/P1000/P1000-1099/P1004 - 方格取数.cpp
@@ -10,31 +10,28 @@ int n;
 int a[10][10];
 int f[10][10][10][10];
 
-int max(int a, int b, int c, int d)
-{
+int max(int a, int b, int c, int d) {
     int e = max(a, b);
     int f = max(c, d);
     return max(e, f);
 }
 
-int main()
-{
+int main() {
     cin >> n;
     int xx, yy, aa;
-    while(cin >> xx >> yy >> aa && (xx || yy || aa))
-    {
+    while(cin >> xx >> yy >> aa && (xx || yy || aa)) {
         a[xx][yy] = aa;
     }
-    for(int i = 1; i <= n; i++)
-    {
-        for(int j = 1; j <= n; j++)
-        {
-            for(int k = 1; k <= n; k++)
-            {
-                for(int l = 1; l <= n; l++)
-                {
-                    if(i == k && j == l)
-                    {
+    for(int i = 1; i <= n; i++) {
+        
+        for(int j = 1; j <= n; j++) {
+            
+            for(int k = 1; k <= n; k++) {
+                
+                for(int l = 1; l <= n; l++) {
+                    
+                    if(i == k && j == l) {
+                        
                         f[i][j][k][l] = max(
                             f[i-1][j][k-1][l],
                             f[i-1][j][k][l-1],
