efe29b23a73730bbd8dd9ace311f5e691ae94921
Yin Yitao <15844309+yin1999@users.noreply.github.com>
2019-09-15 21:52:23 +0800

    Update P1014 - Cantor表.cpp

--- 洛谷/P1000/P1000-1099/P1014 - Cantor表.cpp
+++ 洛谷/P1000/P1000-1099/P1014 - Cantor表.cpp
@@ -2,18 +2,17 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
 	int n;
 	cin >> n;
 	int i = 1;
-	while (i < n)
-	{
+	while (i < n) {
 		n -= i++;
 	}
-	if ((i & 1) == 0)
+	if ((i & 1) == 0) {
 		cout << n << '/' << i + 1 - n << endl;
-	else
+	} else {
 		cout << i + 1 - n << '/' << n << endl;
+	}
 	return 0;
 }
