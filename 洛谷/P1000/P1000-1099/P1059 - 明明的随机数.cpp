2533fc72da07042468b9d3a7fec884c65a7c6098
Yin Yitao <15844309+yin1999@users.noreply.github.com>
2019-09-15 21:56:49 +0800

    Update P1059 - 明明的随机数.cpp

--- 洛谷/P1000/P1000-1099/P1059 - 明明的随机数.cpp
+++ 洛谷/P1000/P1000-1099/P1059 - 明明的随机数.cpp
@@ -2,30 +2,24 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
     int n, a[1001] = { 0 }, temp;
     int count1 = 0;
     cin >> n;
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         cin >> temp;
-        if (a[temp] == 0)    //当此数没有出现过，则计数加一
-        {
+        if (a[temp] == 0) {   //当此数没有出现过，则计数加一
           count1++;
           a[temp] = 1;
         }
     }
     cout << count1 << endl;
     bool flag = false;
-    for (int i = 0; i <= 1000; i++)
-    {
-        if (a[i])
-        {
-            if (flag)
+    for (int i = 0; i <= 1000; i++) {
+        if (a[i]) {
+            if (flag) {
                 cout << ' ' << i;
-            else
-            {
+            } else {
                 cout << i;
                 flag = true;
             }
