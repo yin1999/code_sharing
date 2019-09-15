cc9e5dcf92b97c12b8f73658b0eae2a119107b49
Yin Yitao <15844309+yin1999@users.noreply.github.com>
2019-09-15 21:57:19 +0800

    Update P1075 - 质因数分解.cpp

--- 洛谷/P1000/P1000-1099/P1075 - 质因数分解.cpp
+++ 洛谷/P1000/P1000-1099/P1075 - 质因数分解.cpp
@@ -3,14 +3,11 @@
 using namespace std;
 
 
-int main()
-{
+int main() {
     int n;
     cin >> n;
-    for (int i = 2; i < n; i++)
-    {
-        if (n % i == 0)
-        {
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
             cout << n / i << endl;
             return 0;
         }
