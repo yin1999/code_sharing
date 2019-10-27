7d84b2051f78574dea95eb129aa283be324c4cc4
Yin Yitao <15844309+yin1999@users.noreply.github.com>
2019-10-27 14:19:02 +0800

    Update 示例.cpp

--- ESTA/最大的数/示例.cpp
+++ ESTA/最大的数/示例.cpp
@@ -1,33 +1,27 @@
-#include<iostream>
-#include<cstring>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
 
-int main()
-{
-	int c;
-	cin >> c;     //输入需要处理的案例数
-  
-	while (c--)    //while+自减 进行c次循环
-	{
-		char a[101], b[101];    //a数组储存输入的数，b数组储存处理后的结果
-		int m, count = 0;    //count用来储存现在该在b数组的哪一个位置储存字符了，在内层while循环前初始化为0(处理新的案例)
-		cin >> a >> m;
-		int length = strlen(a), temp = -1;   //temp储存当前寻找的最大数下标，temp=-1的原因参考第20行注释，自己理解
-		m = length - m;    //用m来储存该找出几个数（这样做比删去数组中的数简单）
-    
-		while (m--)   //m自减循环，一次循环找出一个数
-		{
-			for (int i = ++temp ; i <= length - m; i++)    //i=++temp需要好好理解，应为在进行第二次以上的内层循环时，a的下标应向后进一，不能将之前用过的数再用一次
-			{    //i<=length-m 是因为不能找最大数找到这个下标后面，会导致不能找出该有的数的位数（有少一位及以上的风险）
-				if (a[temp] < a[i])
-					temp = i;
+int main() {
+	char num[105], ans[105];
+	int n, del;
+	int length_old, length_new, index, i, j;
+	scanf("%d", &n);
+	while (n--) {
+		scanf("%s%d", num, &del);
+		length_old = strlen(num);
+		length_new = length_old - del;
+		index = 0;
+		for (i = 0; length_new; i++) {
+			for (j = index + 1; j <= length_old - length_new; j++) {
+				if (num[j] > num[index]) {
+					index = j;
+				}
 			}
-			b[count++] = a[temp];
+			ans[i] = num[index];
+			++index;
+			--length_new;
 		}
-    
-		b[count] = '\0';    //字符数组结束符不要忘记
-		cout << b << endl;
+		ans[i] = 0;
+		printf("%s\n", ans);
 	}
-  
-	return 0;
 }
