3e5aa390d98412f5c18795edfc9e60ddc0e3e4df
A1lo <15844309+yin1999@users.noreply.github.com>
2021-11-02 19:59:44 +0800

    Create 237.删除链表中的节点.cpp

---
+++ LeetCode/1-999/200-299/237.删除链表中的节点.cpp
@@ -0,0 +1,15 @@
+// Definition
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+ public:
+  void deleteNode(ListNode* node) {
+    ListNode *next = node->next;
+    *node = *next;
+    delete next; // Memory leak
+  }
+};
